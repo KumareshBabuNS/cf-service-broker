a30bd022cf21c6677eba0033c7d625ff7880603f
Johannes Hiemer <jvhiemer@gmail.com>
2016-03-04 09:38:31 +0100

    Removed due to incorrect path.

--- custom-logstash/src/main�resources/docker/container.cmd
+++
@@ -1,6 +0,0 @@
-export REPOSITORY_LOGSTASH="https://bitbucket.org/evoila-boxer/deployment-scripts-docker-openstack/raw/master/Logstash-v3" &&
-export REPOSITORY_MAIN="https://bitbucket.org/evoila-boxer/deployment-scripts-docker-openstack/raw/master" &&
-apt-get install -y wget && 
-wget https://bitbucket.org/evoila-boxer/deployment-scripts-docker-openstack/raw/master/Logstash-v3/logstash-template.sh &&
-chmod +x logstash-template.sh &&
-./logstash-template.sh -d $es_host -p 3uvjnv3e9823 -e docker
