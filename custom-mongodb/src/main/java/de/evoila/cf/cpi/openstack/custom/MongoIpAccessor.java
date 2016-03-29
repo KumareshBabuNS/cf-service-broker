7f05954311f6f88d802dd5d997c54ac38304b247
Christian Brinker <cbrinker@evoila.de>
2016-03-29 16:33:28 +0200

    made mongodb cluster working

--- custom-mongodb/src/main/java/de/evoila/cf/cpi/openstack/custom/MongoIpAccessor.java
+++ custom-mongodb/src/main/java/de/evoila/cf/cpi/openstack/custom/MongoIpAccessor.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.openstack4j.model.heat.Stack;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import de.evoila.cf.broker.model.ServerAddress;
 import de.evoila.cf.cpi.openstack.OpenstackServiceFactory;
@@ -18,6 +19,7 @@ import jersey.repackaged.com.google.common.collect.Lists;
  * @author Christian Brinker, evoila.
  *
  */
+@Service
 public class MongoIpAccessor extends CustomIpAccessor {
 
 	private HeatFluent heatFluent;
@@ -26,24 +28,27 @@ public class MongoIpAccessor extends CustomIpAccessor {
 	public List<ServerAddress> getIpAddresses(String instanceId) {
 		Stack stack = heatFluent.get(OpenstackServiceFactory.uniqueName(instanceId));
 		List<Map<String, Object>> outputs = stack.getOutputs();
+		List<ServerAddress> serverAddresses = Lists.newArrayList();
 		for (Map<String, Object> output : outputs) {
 			Object outputKey = output.get("output_key");
 			if (outputKey != null && outputKey instanceof String) {
 				String key = (String) outputKey;
-				if (key.equals("ips")) {
+				if (key.equals("sec_ips")) {
+					List<String> outputValue = (List<String>) output.get("output_value");
+
+					for (int i = 0; i < outputValue.size(); i++) {
+						serverAddresses
+								.add(new ServerAddress("sec_ips" + "#" + Integer.toString(i), outputValue.get(i)));
+					}
+				} else if (key.equals("prim_ip")) {
 					String outputValue = (String) output.get("output_value");
 
-					final List<String> ips = Lists.newArrayList(outputValue.split(","));
-					List<ServerAddress> serverAddresses = Lists.newArrayList();
-					for (int i = 0; i < ips.size(); i++) {
-						serverAddresses.add(new ServerAddress(Integer.toString(i), ips.get(i)));
-					}
-					return serverAddresses;
+					serverAddresses.add(new ServerAddress("prim_ip", outputValue));
 				}
 			}
 		}
 
-		return Lists.newArrayList();
+		return serverAddresses;
 	}
 
 	@Autowired
